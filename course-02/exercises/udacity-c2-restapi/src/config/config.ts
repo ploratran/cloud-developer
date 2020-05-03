01d3fb4198ab0e27442a9f7f594d3a67eb7492f1
Plora Tran <ploratran@gmail.com>
2020-05-02 18:06:30 -0700

    zip file, use process.env variables, ready to deploy to EB

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,34 @@
+// export const config = {
+//   "dev": {
+//     "username": "udacityploradev",
+//     "password": "mimimama0129",
+//     "database": "udagramploradev",
+//     "host": "udacityploradb.c4mzfucs0jq6.us-east-2.rds.amazonaws.com",
+//     "dialect": "postgres",
+//     "aws_region": "us-east-2",
+//     "aws_profile": "default",
+//     "aws_media_bucket": "udagramploradev"
+//   },
+//   "prod": {
+//     "username": "udacityploradev",
+//     "password": "mimimama0129",
+//     "database": "udagramploradev",
+//     "host": "udacityploradb.c4mzfucs0jq6.us-east-2.rds.amazonaws.com",
+//     "dialect": "postgres"
+//   }
+// }
+
 export const config = {
-  "dev": {
-    "username": "udacityploradev",
-    "password": "mimimama0129",
-    "database": "udagramploradev",
-    "host": "udacityploradb.c4mzfucs0jq6.us-east-2.rds.amazonaws.com",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
-  },
-  "prod": {
-    "username": "udacityploradev",
-    "password": "mimimama0129",
-    "database": "udagramploradev",
-    "host": "udacityploradb.c4mzfucs0jq6.us-east-2.rds.amazonaws.com",
-    "dialect": "postgres"
+  "postgress": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgress",
+  }, 
+  "aws": {
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
   }
-}
+};
--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { config } from 'dotenv';
 import { sequelize } from './sequelize';
 
 import { IndexRouter } from './controllers/v0/index.router';
