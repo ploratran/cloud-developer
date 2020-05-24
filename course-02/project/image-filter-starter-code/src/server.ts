963b9f8b1b96671dc540bc913ea13fc5fa315eba
Plora Tran <ploratran@gmail.com>
2020-05-24 11:51:00 -0700

    test dev branch

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -33,7 +33,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
