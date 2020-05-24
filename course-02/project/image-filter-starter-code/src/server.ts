34b00465965a9e93456b32aaf96a1563daf49a33
Plora Tran <ploratran@gmail.com>
2020-05-24 13:46:13 -0700

    make config.ts with AWS config

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -47,9 +47,9 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
       res.status(200).sendFile(filteredImgPath, (err) => {
         // deletes any files on the server on finish of the response
         // INPUTS files: Array<string> an array of absolute paths to files
+        if (err) { console.log(err); }
         deleteLocalFiles([filteredImgPath]);
       });
-
     } catch (err) {
       // one error code for caught errors (i.e. 422)
       return res.status(404).send('Error ' + err); 
