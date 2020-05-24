73bf9a8a8001b271bb95bb12dc6a9562f21ae0e4
Plora Tran <ploratran@gmail.com>
2020-05-23 23:16:04 -0700

    unstub JTW auth token success

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -63,28 +63,28 @@ function generateJWT(user: User): string {
 
 // authenticate JWT token
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
-    return next();
-    // console.log('req headers ' + Object.values(req.headers));
-    // if (!req.headers || !req.headers.authorization){
-    //     return res.status(401).send({ message: 'No authorization headers.' });
-    // }
+    //return next();
+    console.log('req headers ' + Object.values(req.headers));
+    if (!req.headers || !req.headers.authorization){
+        return res.status(401).send({ message: 'No authorization headers.' });
+    }
 
-    // // token_bearer: Bearer {{token}}
-    // const token_bearer = req.headers.authorization.split(' ');
+    // token_bearer: Bearer {{token}}
+    const token_bearer = req.headers.authorization.split(' ');
 
-    // if(token_bearer.length != 2){
-    //     // if token is not in the correct format: 
-    //     return res.status(401).send({ message: 'Malformed token.' });
-    // }
+    if(token_bearer.length != 2){
+        // if token is not in the correct format: 
+        return res.status(401).send({ message: 'Malformed token.' });
+    }
     
-    // const token = token_bearer[1];
+    const token = token_bearer[1];
 
-    // return jwt.verify(token, config.jwt.secret, (err, decoded) => {
-    //   if (err) {
-    //     return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
-    //   }
-    //   return next();
-    // });
+    return jwt.verify(token, config.jwt.secret, (err, decoded) => {
+      if (err) {
+        return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+      }
+      return next();
+    });
 }
 
 router.get('/verification', 
