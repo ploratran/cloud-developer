ca9946ebbba29a42f641bbb97efaf1a8dac52588
Plora Tran <ploratran@gmail.com>
2020-06-05 14:00:54 -0700

    continue to confi K8s and Docker files for deployment

--- course-03/exercises/udacity-c3-restapi-feed/src/server.ts
+++ course-03/exercises/udacity-c3-restapi-feed/src/server.ts
@@ -20,7 +20,7 @@ const c = config.dev;
 
   //CORS Should be restricted
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", c.url);
+    res.header("Access-Control-Allow-Origin", c.url); // CORS for frontend 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
@@ -35,7 +35,7 @@ const c = config.dev;
 
   // Start the Server
   app.listen( port, () => {
-      console.log( `server running ` + c.url );
+      console.log( `server running ` + port );
       console.log( `press CTRL+C to stop server` );
   } );
 })();
