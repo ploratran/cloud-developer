56515aa6588083b0eaf269196c4b38f106d0d892
Plora Tran <ploratran@gmail.com>
2020-06-26 00:14:57 -0700

    finish TODO createImage hanlder for aws lambda funciton

--- course-04/exercises/lesson-3/starter-code/src/lambda/http/createImage.ts
+++ course-04/exercises/lesson-3/starter-code/src/lambda/http/createImage.ts
@@ -1,6 +1,7 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
@@ -9,9 +10,13 @@ const imagesTable = process.env.IMAGES_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Caller event', event)
+  
+  // get groupId from url path: 
   const groupId = event.pathParameters.groupId
+  // check group validity: 
   const validGroupId = await groupExists(groupId)
 
+  // return 404 error if image group not exist: 
   if (!validGroupId) {
     return {
       statusCode: 404,
@@ -24,27 +29,58 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     }
   }
 
-  // TODO: Create an image
+  // TODO: Create an image //
+
+  // generate unique id for each image: 
+  const imageId = uuid.v4(); 
+
+  const newImage = await createImage(groupId, imageId, event); 
+  
+  // END TODO //
 
   return {
     statusCode: 201,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
-    body: ''
+    // return JSON object of newly created image: 
+    body: JSON.stringify({
+      newImage,
+    })
   }
 }
 
 async function groupExists(groupId: string) {
-  const result = await docClient
-    .get({
+  const result = await docClient.get({
       TableName: groupsTable,
       Key: {
         id: groupId
       }
-    })
-    .promise()
+    }).promise()
 
   console.log('Get group: ', result)
   return !!result.Item
+}
+
+async function createImage(groupId: string, imageId: string, event: any) {
+
+  const timestamp = new Date().toISOString();
+  const parsedBody = JSON.parse(event.body); 
+
+  const newImage = {
+    groupId, 
+    imageId,
+    timestamp, 
+    ...parsedBody, 
+  }
+
+  // write new item into Dynamodb table:
+  await docClient.put({
+    TableName: imagesTable,
+    Item: newImage
+  }).promise()
+
+  console.log('New image aded: ' + newImage); 
+
+  return newImage; 
 }
