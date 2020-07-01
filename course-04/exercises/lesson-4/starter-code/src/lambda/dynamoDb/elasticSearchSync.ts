f32c6dc11b19a30e12161c76ba5e8e17d6771829
Plora Tran <ploratran@gmail.com>
2020-06-30 20:32:59 -0700

    note on websocket notification + sync dynamodb with elasticsearch

--- course-04/exercises/lesson-4/starter-code/src/lambda/dynamoDb/elasticSearchSync.ts
+++ course-04/exercises/lesson-4/starter-code/src/lambda/dynamoDb/elasticSearchSync.ts
@@ -1,28 +1,36 @@
 import { DynamoDBStreamEvent, DynamoDBStreamHandler } from 'aws-lambda'
 import 'source-map-support/register'
 import * as elasticsearch from 'elasticsearch'
-import * as httpAwsEs from 'http-aws-es'
+import * as httpAwsEs from 'http-aws-es' // dependency to use es in aws
 
-const esHost = process.env.ES_ENDPOINT
 
+const esHost = process.env.ES_ENDPOINT // images-search-dev
+
+// define ElasticSearch client to allow write to ElasticSearch: 
 const es = new elasticsearch.Client({
   hosts: [ esHost ],
   connectionClass: httpAwsEs
 })
 
+// using DynamoDB Event and Handler to process ElasticSearch: 
 export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
   console.log('Processing events batch from DynamoDB', JSON.stringify(event))
 
+  // loop thru all records in DynamoDB Stream Event: 
   for (const record of event.Records) {
     console.log('Processing record', JSON.stringify(record))
     if (record.eventName !== 'INSERT') {
       continue
     }
 
+    // get DynamoDB item that's added to DynamoDB: 
     const newItem = record.dynamodb.NewImage
 
+    // get image ID that's added to Images Table: 
     const imageId = newItem.imageId.S
 
+    // create document that will store in ElasticSearch
+    // derived from DynamoDB: 
     const body = {
       imageId: newItem.imageId.S,
       groupId: newItem.groupId.S,
@@ -31,10 +39,11 @@ export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent)
       timestamp: newItem.timestamp.S
     }
 
+    // store new item to ElasticSearch 
     await es.index({
-      index: 'images-index',
-      type: 'images',
-      id: imageId,
+      index: 'images-index', // index similar to Table in DynamoDB
+      type: 'images', // type of index
+      id: imageId, // document ID
       body
     })
 
