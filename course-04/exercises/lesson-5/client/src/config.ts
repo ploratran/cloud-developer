1ffa2075bfac7533beffd8bb09b4be9bc7f37df0
Plora Tran <ploratran@gmail.com>
2020-07-05 13:08:57 -0700

    configure Auth0 domain, clientId, apiEndpoint in frontend

--- course-04/exercises/lesson-5/client/src/config.ts
+++ course-04/exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://2dqf1fcypa.execute-api.us-east-2.amazonaws.com/dev'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-e5gm3lg4.us.auth0.com',
+  clientId: 'V59FRhxR19FLh4A19Nl0ROiI0v6oZL6P',
   callbackUrl: 'http://localhost:3000/callback'
 }
