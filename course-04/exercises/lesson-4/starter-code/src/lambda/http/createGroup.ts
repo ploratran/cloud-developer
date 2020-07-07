059ac6d39a0736d974cb044b9b634f86a06490eb
Plora Tran <ploratran@gmail.com>
2020-07-06 19:45:21 -0700

    use middy as middleware in createGroup

--- course-04/exercises/lesson-4/starter-code/src/lambda/http/createGroup.ts
+++ course-04/exercises/lesson-4/starter-code/src/lambda/http/createGroup.ts
@@ -1,12 +1,15 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS  from 'aws-sdk'
 import * as uuid from 'uuid'
 
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+
 const docClient = new AWS.DynamoDB.DocumentClient()
 const groupsTable = process.env.GROUPS_TABLE
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
   const itemId = uuid.v4()
 
@@ -24,11 +27,20 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
   return {
     statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
+    // remove because middy will add CORS headers:
+    // headers: {
+    //   'Access-Control-Allow-Origin': '*',
+    //   'Access-Control-Allow-Credentials': true, 
+    // },
     body: JSON.stringify({
       newItem
     })
   }
-}
+})
+
+// use cors middleware: 
+handler.use(cors({
+  // allow headers that allow to send credentials to browser: 
+  credentials: true, 
+}))
+
