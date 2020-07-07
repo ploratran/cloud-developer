ac6088382826093504aa6fbde64f1ec07377294c
Plora Tran <ploratran@gmail.com>
2020-07-07 14:19:00 -0700

    use Domain, ClientID, callbackUrl from Auth0 Udagram App 1 into config.ts

--- course-04/exercises/lesson-4/client/src/config.ts
+++ course-04/exercises/lesson-4/client/src/config.ts
@@ -1,1 +1,8 @@
 export const apiEndpoint = 'https://2dqf1fcypa.execute-api.us-east-2.amazonaws.com/dev'
+
+// plugin Domain, Client ID of Udagram App 1 from auth0.com
+export const authConfig = {
+    domain: 'dev-e5gm3lg4.us.auth0.com', 
+    clientId: 'RO6Thk3DoT3l6Ki9BxLpZwSoVJYuFqto', 
+    callbackUrl: 'http://localhost:3000/callback'
+}
