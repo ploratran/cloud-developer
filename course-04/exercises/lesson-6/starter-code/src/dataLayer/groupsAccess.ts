8f56dd3d150de8a485fcb73436a01ac05a298a87
Plora Tran <ploratran@gmail.com>
2020-07-14 13:18:38 -0700

    disable tracing + stop using aws x-ray in offline mode

--- course-04/exercises/lesson-6/starter-code/src/dataLayer/groupsAccess.ts
+++ course-04/exercises/lesson-6/starter-code/src/dataLayer/groupsAccess.ts
@@ -1,10 +1,10 @@
 // create a class for Groups that used in other files: 
 // blueprint for Groups: 
 import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
+// import * as AWSXRay from 'aws-xray-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 
-const XAWS = AWSXRay.captureAWS(AWS) // debug tool to keep track of user requests
+// const XAWS = AWSXRay.captureAWS(AWS) // debug tool to keep track of user requests
 
 import { Group } from '../models/Group'
 
@@ -44,10 +44,11 @@ export class GroupAccess {
 function createDynamoDBClient() {
   if (process.env.IS_OFFLINE) {
     console.log('Creating a local DynamoDB instance')
-    return new XAWS.DynamoDB.DocumentClient({
+    // return new XAWS.DynamoDB.DocumentClient({
+    return new AWS.DynamoDB.DocumentClient({
       region: 'localhost',
       endpoint: 'http://localhost:8000'
     })
   }
-  return new XAWS.DynamoDB.DocumentClient()
+  return new AWS.DynamoDB.DocumentClient()
 }
