e563f9053e4265dadf375cde14cf394ceb86e94c
Plora Tran <ploratran@gmail.com>
2020-07-15 20:11:49 -0700

    apply clientId and domain from Auth0

--- course-04/exercises/lesson-6/client/src/config.ts
+++ course-04/exercises/lesson-6/client/src/config.ts
@@ -1,7 +1,8 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://xl2jzwm28b.execute-api.us-east-2.amazonaws.com/dev/'
 
+// get clientID and Domain from Auth0.com
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-a6-tsbmc.us.auth0.com',
+  clientId: '23NkoE1v37qsq7brtoieUnbX2dqC6Q6Y',
   callbackUrl: 'http://localhost:3000/callback'
 }
