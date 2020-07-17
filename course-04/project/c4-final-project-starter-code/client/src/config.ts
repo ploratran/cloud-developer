25d74317275007e4843edc78b1bd867456ec4ea7
Plora Tran <ploratran@gmail.com>
2020-07-17 16:01:01 -0700

    domain and clientID for frontend config.ts

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-a6-tsbmc.us.auth0.com', // Auth0 domain
+  clientId: 'hFGLqXuYSN3KoAWpImwodFNiJnsXA0XF',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
