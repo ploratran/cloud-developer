0e00cf52616a52d7d59d10dbc89f0c2e966ca0b0
Plora Tran <ploratran@gmail.com>
2020-07-16 17:26:41 -0700

    change url expired to number value of 300

--- course-04/exercises/lesson-6/starter-code/src/lambda/http/createImage.ts
+++ course-04/exercises/lesson-6/starter-code/src/lambda/http/createImage.ts
@@ -16,7 +16,7 @@ const s3 = new XAWS.S3({
 const groupsTable = process.env.GROUPS_TABLE
 const imagesTable = process.env.IMAGES_TABLE
 const bucketName = process.env.IMAGES_S3_BUCKET
-const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+// const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Caller event', event)
